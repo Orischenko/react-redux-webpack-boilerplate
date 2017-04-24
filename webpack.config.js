4387f197e42848d3327e89eac809647a89aef3b4
Alexander <Lynx@Alexanders-MacBook-Pro.local>
2017-04-24 22:59:09 +0300

    react-hmr was deleted, NODE_ENV was added in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDevelopment = NODE_ENV === 'development';
+
 module.exports = {
-    devtool: 'source-map',
+
+    //watch: isDevelopment,
+    devtool: isDevelopment && 'eval-source-map',
 
     entry: [
         './src/app.js'
@@ -32,5 +37,13 @@ module.exports = {
                 include: path.join(__dirname, 'src')
             }
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(NODE_ENV)
+            }
+        })
+    ]
 };
